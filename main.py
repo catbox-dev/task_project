f68565b608ef26e35f72f3c8263b3688b956d7b0
catbox-dev <catbox.py@gmail.com>
2023-11-07 16:40:06 -0600

    add menu

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 # Create task list
 
 # Add functions for task managment
+def print_menu():
+    print('Menu')
+    print('1. Add task')
+    print('2. Complete task')
+    print('3. Show task list')
+    print('4. Exit\n')
 
 # Create main function
+def main():
+    print_menu()
 
 # Call main function
+if __name__ == "__main__":
+    main()
